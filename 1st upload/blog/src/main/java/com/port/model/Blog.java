95a51435bdbdd53e590454723c12a18ea35b0306
kalaivanan94 <s.kalaivanan007@gmail.com>
2016-11-10 09:29:41 +0530

    page creation

---
+++ 1st upload/blog/src/main/java/com/port/model/Blog.java
@@ -0,0 +1,30 @@
+package com.port.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Blog {
+
+	
+		@Id
+	    @GeneratedValue(strategy=GenerationType.AUTO)
+		private int userid;
+		private int uname;
+		public int getUserid() {
+			return userid;
+		}
+		public void setUserid(int userid) {
+			this.userid = userid;
+		}
+		public int getUname() {
+			return uname;
+		}
+		public void setUname(int uname) {
+			this.uname = uname;
+		}
+
+}
+
